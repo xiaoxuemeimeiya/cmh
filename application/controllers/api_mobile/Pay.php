368fe11cc5e4c34fad41aeddfa0c7610fe74d92e
lxm <1027093208@qq.com>
2021-05-28 16:27:30 +0800

    update

--- application/controllers/api_mobile/Pay.php
+++ application/controllers/api_mobile/Pay.php
@@ -234,7 +234,7 @@ class Pay extends CI_Controller
         $Receivers = [
             'type' => 'MERCHANT_ID',
             'account' => '1608890757',
-            'amount' => 2,
+            'amount' => 200,
             'description' => '分账'
         ];
 
