11dd8c6cd88eb97c9cdf3207a566af3464c2b21f
lixuemei <1027093208@qq.com>
2021-10-31 17:16:18 +0800

    update

--- application/controllers/api_mobile/goods/Index.php
+++ application/controllers/api_mobile/goods/Index.php
@@ -254,7 +254,7 @@ class Index extends MY_Controller
             echo ch_json_encode($this->ResArr);exit;
         }
         //获取该详情的店铺
-        $detail = $this->loop_model('member_shop')->field('shop_name,logo')->where('id',$id)->find();
+        $detail = $this->loop_model->get_id('member_shop',$id,'shop_name,logo');
         $item['shop_name'] = $detail['name'];
         $item['logo'] = $detail['logo'];
         $this->ResArr["code"] = 200;
